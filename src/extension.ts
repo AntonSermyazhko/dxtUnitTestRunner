e18d1f774d7f2b9b2672be1a0f6591b8dc0d00be
CORP\sermyazhko.anton <anton.sermyazhko@devexpress.com>
2019-07-23 11:40:32 +0300

    Add running test from the test body

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,11 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as browserTools from 'testcafe-browser-tools';
 import * as isReachable from 'is-reachable';
+import { type } from 'os';
 
 const BROWSER_ALIASES = ['ie', 'firefox', 'chrome', 'chrome-canary', 'chromium', 'opera', 'safari', 'edge'];
-const HEADLESS_MODE_POSTFIX = ":headless";
 
-let qunitController: QUnitTestController = null;
+var qunitController: QUnitTestController = null;
 
 function registerRunQUnitTestsCommands (context:vscode.ExtensionContext){
     context.subscriptions.push(
@@ -23,7 +23,7 @@ function registerRunQUnitTestFileCommands (context:vscode.ExtensionContext) {
     context.subscriptions.push(
         vscode.commands.registerCommand('qunitrunner.runTestFileInChrome', args => {
             console.log("qunitrunner.runTestFileInChrome", args);
-            qunitController.startTestRun("chrome", args.fsPath, "", "file");
+            qunitController.startTestRun("chrome", args.fsPath, new TestInfo('file', "", ""));
         })
     );
 }
@@ -65,25 +65,25 @@ export function deactivate() {
 
 class QUnitTestController {
     public runTests(browser:string) {
-        let editor = vscode.window.activeTextEditor;
-        if(editor) {
-            var doc = editor.document;
-            if(doc.languageId === "javascript" || doc.languageId === "typescript") {
-                let document = editor.document,
-                    selection = editor.selection;
+        var editor = vscode.window.activeTextEditor,
+            document = editor && editor.document
+        if(!editor || !document) {
+            return;
+        }
+        if(document.languageId === "javascript" || document.languageId === "typescript") {
+            let selection = editor.selection;
+            if(selection && selection.active) {
+                let textLine = document.lineAt(selection.active.line).text,
+                    cursorPosition = document.getText(new vscode.Range(0, 0, selection.active.line, selection.active.character)).length,
+                    textBeforeSelection = document.getText(new vscode.Range(0, 0, selection.end.line + 1, 0)),
+                    testInfo = this.findTest(textBeforeSelection, textLine, cursorPosition);
 
-                if(selection && selection.active) {
-                    let textLine = document.lineAt(selection.active.line).text,
-                        textBeforeSelection = document.getText(new vscode.Range(0, 0, selection.end.line + 1, 0)),
-                        [type, module, name] = this.findTestOrFixtureName(textBeforeSelection, textLine);
-
-                    this.startTestRun(browser, document.fileName, type, module, name);
-                }
+                this.startTestRun(browser, document.fileName, testInfo);
             }
         }
     }
 
-    public startTestRun(browser: string, filePath: string, type: string, moduleName: string, testName: string = "") {
+    public startTestRun(browser: string, filePath: string, testInfo: TestInfo) {
         if(!type) {
             vscode.window.showErrorMessage(`No tests found. Position the cursor on the test caption.`);
             return;
@@ -91,6 +91,7 @@ class QUnitTestController {
 
         var rootPathMatch = /.+?(?=testing)/.exec(filePath);
         if(!rootPathMatch) {
+            vscode.window.showErrorMessage(`Test runner: Error parse test file root path`);
             return;
         }
 
@@ -101,42 +102,48 @@ class QUnitTestController {
                 return;
             }
 
+            console.log(`Running test: module=${testInfo.module}, test=${testInfo.name}`);
+
             var ports = JSON.parse(data),
                 qunitPort = ports.qunit,
                 qunitUrl: string = `http://localhost:${qunitPort}`;
 
-            isReachable(qunitUrl, { timeout: 200 }).then(result => {
+            isReachable(qunitUrl, { timeout: 400 }).then(result => {
                 var relativeFilePathMatch = /(?:testing\\tests\\)(.*)/.exec(filePath);
                 if(relativeFilePathMatch) {
                     if(!result) {
-                        var terminal = vscode.window.createTerminal("qunit test runner");
-                        if(terminal) {
-                            terminal.show(true);
-                            terminal.sendText("cd ./testing");
-                            terminal.sendText("node launch");
-                            isReachable(qunitUrl, { timeout: 10000 }).then(result => {
-                                if(!result) {
+                        if(this.runQUnitTestService()) {
+                            isReachable(qunitUrl, { timeout: 30000 }).then(result => {
+                                if(result) {
+                                    setTimeout(() => this.runTest(browser, qunitPort, filePath, testInfo.module, testInfo.name), 2000);
+                                } else {
                                     vscode.window.showErrorMessage(`Test runner: Error start testing service`);
-                                    return;
                                 }
-                                setTimeout(() => this.runTest(browser, qunitPort, filePath, moduleName, testName), 2000);
                             });
                         }
                     } else {
-                        this.runTest(browser, qunitPort, filePath, moduleName, testName);
+                        this.runTest(browser, qunitPort, filePath, testInfo.module, testInfo.name);
                     }
                 }
             });
         });
     }
 
-    private findTestOrFixtureName(textBeforeSelection: string, textLine: string): string[] {
-        const QUNIT_TEST_RE = /(^|;|\s+|\/\/|\/\*)QUnit\.test\s*(?:\.[a-zA-Z]+\([^\)]*\))*\s*\(\s*(.+?\s*('|"))\s*,/gm;
-        const QUNIT_MODULE_RE = /(^|;|\s+|\/\/|\/\*)QUnit\.module\s*\(\s*('|")(.+?\s*)('|")\s*,/gm;
-        const CLEANUP_TEST_OR_FIXTURE_NAME_RE = /(^\(?\s*(\'|"|`))|((\'|"|`)\s*\)?$)/g;
+    private runQUnitTestService(): boolean {
+        var terminal = vscode.window.createTerminal("qunit test runner");
+        if(terminal) {
+            terminal.show(true);
+            terminal.sendText("cd ./testing");
+            terminal.sendText("node launch");
+            return true;
+        }
+        vscode.window.showErrorMessage(`Test runner: Error run QUnit test service`);
+        return false;
+    }
 
-        var match = QUNIT_TEST_RE.exec(textLine),
-            moduleMatch = QUNIT_MODULE_RE.exec(textBeforeSelection),
+    private findModule(textBeforeSelection: string): string {
+        const QUNIT_MODULE_RE = /(^|;|\s+|\/\/|\/\*)QUnit\.module\s*\(\s*('|")(.+?\s*)('|")\s*/gm;
+        var moduleMatch = QUNIT_MODULE_RE.exec(textBeforeSelection),
             moduleName = "";
 
         while(moduleMatch !== null) {
@@ -144,10 +151,69 @@ class QUnitTestController {
             moduleMatch = QUNIT_MODULE_RE.exec(textBeforeSelection);
         }
 
-        if(match) {
-            return ['test', moduleName, match[2].replace(CLEANUP_TEST_OR_FIXTURE_NAME_RE, '')];
+        if(!moduleName) {
+            vscode.window.showWarningMessage(`Test runner: Test module not found`);
         }
-        return ['', '', ''];
+
+        return moduleName;
+    }
+
+    private findTest(textBeforeSelection: string, textLine: string, cursorPosition: number): TestInfo {
+        const QUNIT_TEST_RE = /(^|;|\s+|\/\/|\/\*)QUnit\.test\s*(?:\.[a-zA-Z]+\([^\)]*\))*\s*\(\s*(.+?\s*('|"))\s*,/gm;
+        const CLEANUP_TEST_OR_FIXTURE_NAME_RE = /(^\(?\s*(\'|"|`))|((\'|"|`)\s*\)?$)/g;
+
+        var testMatch = QUNIT_TEST_RE.exec(textLine),
+            matches = [],
+            moduleName = this.findModule(textBeforeSelection),
+            lastOneTest = null;
+
+        if(testMatch) {
+            let testName = testMatch[2].replace(CLEANUP_TEST_OR_FIXTURE_NAME_RE, '');
+            return new TestInfo('test', moduleName, testName);
+        } else {
+            testMatch = QUNIT_TEST_RE.exec(textBeforeSelection);
+            while(testMatch !== null) {
+                if(this.isTest(testMatch[0])) {
+                    let name = testMatch[2],
+                        realIndex = testMatch.index + testMatch[0].length - this.cropMatchString(testMatch[0]).length;
+                    matches.push({
+                        type: 'test',
+                        module: moduleName,
+                        name: name.replace(CLEANUP_TEST_OR_FIXTURE_NAME_RE, ''),
+                        index: realIndex
+                    });
+                }
+
+                testMatch = QUNIT_TEST_RE.exec(textBeforeSelection);
+            }
+
+            if(matches.length){
+                for(var i = matches.length - 1; i >= 0; i--){
+                    if(cursorPosition >=  matches[i].index){
+                        lastOneTest = matches[i];
+                        break;
+                    }
+                }
+            }
+
+            if(lastOneTest) {
+                return new TestInfo(lastOneTest.type, lastOneTest.module, lastOneTest.name);
+            }
+        }
+
+        return new TestInfo("", "", "");
+    }
+
+    private isTest(matchString) {
+        var validPrefixes = ['QUnit.test', 'QUnit.testInActiveWindow'],
+            cropedString = this.cropMatchString(matchString);
+        return !!validPrefixes.find(item => cropedString.indexOf(item));
+    }
+
+    private cropMatchString(matchString){
+        matchString = matchString.trim().replace(/;|\/\/|\/\*/, '');
+
+        return matchString.trim();
     }
 
     private runTest(browser: string, qunitPort: number, filePath: string, moduleName: string, testName: string) {
@@ -179,5 +245,17 @@ class QUnitTestController {
     }
 
     dispose() {
+    }
+}
+
+class TestInfo {
+    type: string;
+    module: string;
+    name: string;
+
+    constructor(type: string, module: string, name: string) {
+        this.type = type;
+        this.module = module;
+        this.name = name;
     }
 }
